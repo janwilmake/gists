ab9f2d39122e00105cc1c3e7f8806b74edc1ce36
Jan Wilmake <jan@wilmake.com>
2025-04-21 15:33:49 +0200

    improvements

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 /**
  * Ensure @cloudflare/workers-types is accessible. `npm i --save-dev @cloudflare/workers-types`
  *
- * Get type 'safety' in js projects
+ * Get type 'safety' in js projects in VSCode (and other IDEs with good typescript support). Why js > ts? Because it runs in browsers too and with things like `eval` (without bundling/compilation).
  */
-/// <reference types="@cloudflare/workers-types" />
 //@ts-check
+/// <reference types="@cloudflare/workers-types" />
 export default {
   /**
    * Cloudflare Worker handler function
