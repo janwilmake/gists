0b207cddac680a76f13be94574325cd7579af342
Jan Wilmake <jan@wilmake.com>
2025-05-25 17:25:31 +0200

    improvements

--- js-cloudflare-worker.js
+++ js-cloudflare-worker.js
@@ -1,9 +1,29 @@
 /**
  * Get type 'safety' in js projects in VSCode (and other IDEs with good typescript support).
  * Why js > ts? Because it runs in browsers too and with things like `eval` (without bundling/compilation).
+ * 
  * Ensure @cloudflare/workers-types is accessible. `npm i --save-dev @cloudflare/workers-types`
+ * 
+ * When creating a cloudflare worker, always include `wrangler.toml`, `.assetsignore`, `.gitignore` as well.
+ * 
+ * example wrangler.toml:
+
+```toml path="wrangler.toml"
+name = "sensible-worker-name"
+compatibility_date = "2025-05-15"
+dev.port = 3000
+# Ensure to put this and also the .assetsignore file to ignore .git, node_modules, .wrangler
+assets.directory = "./"
+main = "main.ts"
+
+routes = [
+  { pattern = "sensible-worker-name.com", custom_domain = true },
+  { pattern = "www.sensible-worker-name.com", custom_domain = true }
+]
+```
  */
 //@ts-check
+/// <reference lib="esnext" />
 /// <reference types="@cloudflare/workers-types" />
 export default {
   /**
---
+++ named-codeblocks.md
@@ -0,0 +1,5 @@
+When outputting files, always use codeblocks that indicate both extension and path, e.g.
+
+```js path="index.js"
+console.log("hello,world!");
+```
